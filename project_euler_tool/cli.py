8530493183d348699ad7a0aefb1a6f788d3e6473
Marcelo Fornet <mfornet94@gmail.com>
2023-10-30 12:13:42 +0100

    Richer dsl for queries

--- project_euler_tool/cli.py
+++ project_euler_tool/cli.py
@@ -1,4 +1,7 @@
+import re
+import sys
 from collections import defaultdict
+from functools import partial
 
 from dotenv import load_dotenv
 from tabulate import tabulate
@@ -6,10 +9,7 @@ from tabulate import tabulate
 from .parse import parse_progress
 
 
-def main():
-    load_dotenv()
-    problems = parse_progress()
-
+def default(problems):
     n_cat = 3
     n_diff_cat = 2
 
@@ -34,5 +34,70 @@ def main():
     selected_problems = sorted(
         selected_problems.values(), key=lambda p: (p.difficulty, -p.authors)
     )
+
+    return selected_problems
+
+
+def get(p, id):
+    if id == "pid":
+        return p.problem_id
+    elif id.startswith("diff"):
+        return p.difficulty
+    elif id.startswith("author"):
+        return p.authors
+    else:
+        return int(id)
+
+
+AVAILABLE_OPS = ["<", "<=", ">", ">=", "=="]
+OPS = "|".join(AVAILABLE_OPS)
+
+
+def compare(left, op, right):
+    assert isinstance(left, int)
+    assert isinstance(right, int)
+    assert op in AVAILABLE_OPS
+    return eval(f"{left} {op} {right}")
+
+
+FIELDS = "pid|diff[a-z]*|authors?"
+
+
+def parse_filter(f):
+    solved = re.match("^(not )?solved$", f)
+    if solved:
+        want_solved = solved.group(1) is None
+        fn = lambda p: p.solved == want_solved
+        return partial(filter, fn)
+
+    comparison = re.match(f"^({FIELDS}|[0-9]+) ({OPS}) ({FIELDS}|[0-9]+)$", f)
+    if comparison:
+        left = comparison.group(1)
+        op = comparison.group(2)
+        right = comparison.group(3)
+        fn = lambda p: compare(get(p, left), op, get(p, right))
+        return partial(filter, fn)
+
+    sort = re.match(f"^sort ({FIELDS})( asc| desc)?$", f)
+    if sort:
+        reverse = sort.group(2) == " desc"
+        fn = lambda ps: sorted(ps, key=lambda p: get(p, sort.group(1)), reverse=reverse)
+        return fn
+
+    return lambda x: x
+
+
+def main():
+    load_dotenv()
+    problems = parse_progress()
+
+    if len(sys.argv) == 2:
+        for f in sys.argv[1].split("|"):
+            f = f.strip(" ").lower()
+            fn = parse_filter(f)
+            problems = fn(problems)
+        selected_problems = problems
+    else:
+        selected_problems = default(problems)
 
     print(tabulate(selected_problems, headers="keys"))
